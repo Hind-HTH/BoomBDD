703290a15d9c0286717deb913b9409071cdb3e26
Hind-HTH <102022233+Hind-HTH@users.noreply.github.com>
2024-01-24 14:51:40 +0100

    Les requetes

--- H3hitema.sql
+++ H3hitema.sql
@@ -6,33 +6,34 @@ Create table Session(
     id int primary key  AUTO_INCREMENT,
     niveau varchar(50),
     spécialité varchar(50)
-);
+)ENGINE=InnoDB;
 
 create table Prof(
     id int primary key AUTO_INCREMENT,
     nom varchar(50),
     prenom varchar(50)
-);
+)ENGINE=InnoDB;
 
 create table Salle(
     id int primary key AUTO_INCREMENT,
     étage varchar(50),
     numero_classe int
-);
+)ENGINE=InnoDB;
 
-create table Cours(
-    id int primary key AUTO_INCREMENT,
-    nom_cours varchar(50),
-    id_prof int,
-    id_salle int,
-    foreign key(id_prof) references Prof(id),
-    foreign key(id_salle) references Salle(id)
-);
+CREATE TABLE Cours (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom_cours VARCHAR(50),
+    id_prof INT,
+    id_salle INT,
+    FOREIGN KEY (id_prof) REFERENCES Prof(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_salle) REFERENCES Salle(id) ON DELETE CASCADE
+)ENGINE=InnoDB;
+
 
 create table evaluation(
     id int primary key AUTO_INCREMENT,
     nom varchar(50)
-);
+)ENGINE=InnoDB;
 
 create table Etudiants(
     id int primary key AUTO_INCREMENT,
@@ -45,25 +46,25 @@ create table Etudiants(
     ville varchar(50),
     codePostale varchar(50),
     id_session int,
-    foreign key (id_session) references Session(id)
-);
+    foreign key (id_session) references Session(id) ON DELETE CASCADE
+)ENGINE=InnoDB;
 
 create table Notes(
     notes int,
     id_etudiants int,
     id_evaluation int,
-    foreign key (id_etudiants) references Etudiants(id),
-    foreign key (id_evaluation) references Evaluation(id),
-    primary key (id_etudiants, id_evaluation)
-);
+    primary key (id_etudiants, id_evaluation),
+    foreign key (id_etudiants) references Etudiants(id) ON DELETE CASCADE,
+    foreign key (id_evaluation) references Evaluation(id) ON DELETE CASCADE
+)ENGINE=InnoDB;
 
-create table Suivre(
-    id_etudiants int,
-    id_cours int,
-    foreign key (id_etudiants) references Etudiants(id),
-    foreign key (id_cours) references Cours(id),
-    primary key (id_etudiants, id_cours)
-);
+CREATE TABLE `Suivre` (
+  `id_etudiants` int(11) NOT NULL,
+  `id_cours` int(11) NOT NULL,
+  PRIMARY KEY (`id_etudiants`, `id_cours`),
+  FOREIGN KEY (`id_etudiants`) REFERENCES `Etudiants` (`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`id_cours`) REFERENCES `Cours` (`id`) ON DELETE CASCADE
+)ENGINE=InnoDB;
 
 
 INSERT INTO Session (niveau, spécialité) VALUES ('Bachelor', 'Informatique');
@@ -131,3 +132,96 @@ INSERT INTO Suivre (id_etudiants, id_cours) VALUES (2, 2);
 INSERT INTO Suivre (id_etudiants, id_cours) VALUES (3, 3);
 INSERT INTO Suivre (id_etudiants, id_cours) VALUES (4, 4);
 INSERT INTO Suivre (id_etudiants, id_cours) VALUES (5, 5);
+
+
+
+----------------------------les requêtes-------------------------------
+
+-----Alter table------
+alter table etudiants add age varchar(50);
+
+-----Drop table------
+Drop table Etudiants;
+
+-----Update------
+update etudiants set nom='hind' where id = 1;
+
+-----Delete------
+DELETE FROM etudiants WHERE id= 5;
+
+-----Group By------
+select nom, prenom , count(codePostale)
+from etudiants
+GROUP BY codePostale;
+
+select id_etudiants,count(notes)
+from notes
+where notes >90
+GROUP BY notes;
+
+----- ORDER BY------
+select * from etudiants ORDER by id;
+
+----- inner join------
+select nom, prenom, notes 
+from etudiants
+inner JOIN notes where etudiants.id = notes.id_etudiants;
+
+----- HAVING---comme where seuelemnt having pour sum, count, min, max---
+select nom, prenom
+from etudiants 
+INNER join notes 
+where etudiants.id= notes.id_etudiants
+GROUP by nom
+having sum(notes>=80);
+
+-----UNION------
+select * from prof
+UNION
+select * from session;
+
+
+-----LIMIT------
+select * from etudiants LIMIT 2;
+
+
+-----DISTINCT-enlever la redandance-----
+select DISTINCT prenom from etudiants;
+
+
+-----LIKE--like avec %----
+select * from etudiants where prenom like '%e';
+
+
+-----IN------
+select prenom from  etudiants where prenom in ('Alice', 'Hugo');
+
+
+-----BETWEEN------
+select notes from notes WHERE notes BETWEEN '85' and '95';
+
+
+-----CASE------
+SELECT
+    etudiants.nom,
+    etudiants.prenom,
+    notes.notes,
+    CASE
+        WHEN notes.notes >= 16 THEN 'Très bien'
+        WHEN notes.notes >= 14 THEN 'Bien'
+        WHEN notes.notes >= 12 THEN 'Assez bien'
+        WHEN notes.notes >= 10 THEN 'Passable'
+        ELSE 'Insuffisant'
+    END AS appreciation
+FROM
+    etudiants
+INNER JOIN
+    notes ON etudiants.id = notes.id_etudiants;
+
+
+-----TRUNCATE TABLE------
+TRUNCATE TABLE `notes`;
+
+
+-----EXPLAIN------
+EXPLAIN SELECT * FROM `etudiants`;
